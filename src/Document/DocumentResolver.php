21d6ae8577fd400cc406f08cb1414e5f06962eac
andrii.svirin <andrii.svirin@danads.se>
2023-02-15 17:37:31 +0200

    Add encoding detect

--- src/Document/DocumentResolver.php
+++ src/Document/DocumentResolver.php
@@ -11,24 +11,37 @@ use DOMDocument;
  */
 final class DocumentResolver
 {
+  private const ENCODING_DEFAULT = 'utf-8';
+
   public function resolve(string $html): ?DOMDocument
   {
     $dom = new DOMDocument;
 
     $dom->substituteEntities = false;
 
-    $sourceUtf8 = mb_convert_encoding($html, 'html-entities', 'utf-8');
+    $detect = $this->detectEncoding($html);
 
-    if (empty($sourceUtf8)) {
+    if (self::ENCODING_DEFAULT !== $detect) {
+      $html = mb_convert_encoding($html, self::ENCODING_DEFAULT, $detect);
+    }
+
+    if (empty($html)) {
       return null;
     }
 
-    $load = @$dom->loadHTML($sourceUtf8, LIBXML_HTML_NOIMPLIED | LIBXML_HTML_NODEFDTD);
+    $load = @$dom->loadHTML($html, LIBXML_HTML_NOIMPLIED | LIBXML_HTML_NODEFDTD);
 
     if (!$load) {
       return null;
     }
 
     return $dom;
+  }
+
+  private function detectEncoding(string $html): string
+  {
+    preg_match('/charset=([^ \"]*)/', $html, $matches);
+
+    return isset($matches[1]) ? strtolower($matches[1]) : self::ENCODING_DEFAULT;
   }
 }
